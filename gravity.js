4bce87d40643106097fb52361b6fb30ab72c00d7
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-06-21 01:53:03 +0200

    done

--- gravity.js
+++ gravity.js
@@ -8,7 +8,6 @@ class Entity {
     this.positionX = entityInfo.positionX;
     this.width = entityInfo.width;
     this.height = entityInfo.height;
-    this.mass = entityInfo.mass;
     this.color = entityInfo.color;
     this.info = entityInfo;
   }
@@ -20,14 +19,14 @@ class Entity {
     e.style.top = this.positionY + 'px';
     e.style.left = this.positionX + 'px';
     e.style.backgroundColor = this.color;
-    console.log(this.info);
     return e;
   }
 }
 class Game {
-  constructor(frames, gravity = 9.8) {
+  constructor(frames, a = 9.8, ground) {
     this.frames = frames;
-    this.gravity = gravity;
+    this.a = a;
+    this.ground = ground;
   }
   animate(func) {
     setInterval(() => {
@@ -35,17 +34,32 @@ class Game {
     }, 1000 / this.frames);
   }
   gravity() {
-    this.animate(() => {});
+    let posY = 10;
+    let aPerTime = this.a / this.frames;
+    this.animate(() => {
+      gameContainer.innerHTML = '';
+      let entityInfo = {
+        name: 'player-entity',
+        positionX: 10,
+        positionY: posY,
+        width: 50,
+        height: 50,
+        color: 'green',
+      };
+      aPerTime += this.a / this.frames;
+      console.log(aPerTime);
+      let playerEntity = new Entity(entityInfo);
+      let player = playerEntity.renderEntity();
+      if (posY >= this.ground) {
+        posY = this.ground;
+      } else {
+        posY = posY + aPerTime;
+      }
+      // console.log(posY);
+      gameContainer.appendChild(player);
+    });
   }
 }
-let entityInfo = {
-  name: 'player-entity',
-  positionX: 0,
-  positionY: 0,
-  width: 50,
-  height: 50,
-  mass: 1,
-  color: 'red',
-};
-let player = new Entity(entityInfo);
-gameContainer.appendChild(player.renderEntity());
+let ground = gameContainer.offsetHeight - 50;
+let world = new Game(60, 9.8, ground);
+world.gravity();
