3bac09a3950fd7e6d771d50baa490b910813de47
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-06-20 09:50:23 +0200

    add files

---
+++ gravity.js
@@ -0,0 +1,32 @@
+class Entity {
+  constractor(entityInfo) {
+    this.name = entityInfo.name;
+    this.position.y = entityInfo.positionY;
+    this.position.x = entityInfo.positionX;
+    this.width = entityInfo.width;
+    this.height = entityInfo.height;
+    this.mass = entityInfo.mass;
+    this.color = entityInfo.color
+  }
+  renderEntity(){
+    const e = document.createElement('div')
+    e.classList.add(this.name)
+    e.style.width = this.width + 'px'
+    e.style.height = this.height + 'px'
+    e.style.backgroundColor = this.color
+  }
+}
+class Game {
+  constractor(frames, gravity = 9.8) {
+    this.frames = frames;
+    this.gravity = gravity;
+  }
+  animate(func) {
+    setInterval(() => {
+      func();
+    }, 1000 / this.frames);
+  }
+  gravity() {
+    this.animate(() => {});
+  }
+}
